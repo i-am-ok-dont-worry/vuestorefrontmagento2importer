50b69452e6f8fcf767938cc0deb25eef39aee4c4
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-08 17:30:22 +0100

    Product indexer update

--- src/adapters/magento/product_new.js
+++ src/adapters/magento/product_new.js
@@ -1,5 +1,7 @@
 'use strict';
+
 let AbstractMagentoAdapter = require('./abstract');
+const CacheKeys = require('./cache_keys');
 const util = require('util');
 
 class ProductNewAdapter extends AbstractMagentoAdapter {
@@ -93,11 +95,13 @@ class ProductNewAdapter extends AbstractMagentoAdapter {
 
     preProcessItem(item) {
         return this.api.productsNew.single(item.sku)
-            .then((product) => {
+            .then(async (product) => {
                 this.mapCustomAttributesToObjectRoot(product);
                 this.processStocks(product);
                 this.processMedia(product);
                 this.processBundleOptions(product);
+                await this.processConfigurableOptions(product);
+                await this.processCategories(product);
 
                 logger.info(`Product ${product.sku} imported`);
                 return product;
@@ -147,11 +151,11 @@ class ProductNewAdapter extends AbstractMagentoAdapter {
             let media_gallery = [];
             for (let media of item.media_gallery_entries || []) {
                 media_gallery.push({
-                    image: mediaItem.file,
-                    pos: mediaItem.position,
-                    typ: mediaItem.media_type,
-                    lab: mediaItem.label,
-                    vid: this.computeVideoData(mediaItem)
+                    image: media.file,
+                    pos: media.position,
+                    typ: media.media_type,
+                    lab: media.label,
+                    vid: this.computeVideoData(media)
                 });
             }
 
@@ -159,7 +163,7 @@ class ProductNewAdapter extends AbstractMagentoAdapter {
             delete item['media_gallery_entries'];
             return item;
         } catch (e) {
-            logger.warn(`Unable to retrieve media gallery info`);
+            logger.warn(`Unable to retrieve media gallery info: `, e);
             return item;
         }
     }
@@ -182,6 +186,181 @@ class ProductNewAdapter extends AbstractMagentoAdapter {
             logger.warn(`Unable to retrieve bundle options info`);
             return item;
         }
+    }
+
+    /**
+     * Processes configurable options
+     * Expands configurable children
+     * @param item Current product
+     * @returns Product
+     */
+    async processConfigurableOptions (item) {
+        if (item.type_id !== 'configurable') { return item; }
+        try {
+            const children = await this.api.configurableChildren.list(item.sku);
+            const configurable_children = new Array();
+
+            for (let prOption of children) {
+                let confChild = {
+                    sku: prOption.sku,
+                    id: prOption.id,
+                    status: prOption.status,
+                    visibility: prOption.visibility,
+                    name: prOption.name,
+                    price: prOption.price,
+                    tier_prices: prOption.tier_prices,
+                };
+
+                if (prOption.custom_attributes) {
+                    for (let opt of prOption.custom_attributes) {
+                        confChild[opt.attribute_code] = opt.value
+                    }
+                }
+
+                const context = this.current_context;
+
+                if (context.renderedProducts && context.renderedProducts.items.length) {
+                    const renderedProducts = context.renderedProducts;
+                    const subProductAdditionalInfo = renderedProducts.items.find(p => p.id === confChild.id);
+
+                    if (subProductAdditionalInfo && subProductAdditionalInfo.price_info) {
+                        delete subProductAdditionalInfo.price_info.formatted_prices;
+                        delete subProductAdditionalInfo.price_info.extension_attributes;
+                        confChild = Object.assign(confChild, subProductAdditionalInfo.price_info);
+                        if (confChild.final_price < confChild.price) {
+                            confChild.special_price = confChild.final_price;
+                        }
+
+                        if(this.config.product.renderCatalogRegularPrices) {
+                            confChild.price = confChild.regular_price;
+                        }
+
+                    }
+                }
+
+                configurable_children.push(confChild);
+
+                if (item.price  == 0) { // if price is zero fix it with first children
+                    item.price = prOption.price;
+                }
+
+                Object.assign(item, { configurable_children });
+
+                // EXPAND CONFIGURABLE CHILDREN ATTRS
+                if (this.config.product && this.config.product.expandConfigurableFilters) {
+                    for (const attrToExpand of this.config.product.expandConfigurableFilters){
+                        const expandedSet = new Set();
+                        if (item[attrToExpand]) {
+                            expandedSet.add(item[attrToExpand]);
+                        }
+                        for (const confChild of item.configurable_children) {
+                            if (confChild[attrToExpand]) {
+                                expandedSet.add(confChild[attrToExpand]);
+                            }
+                        }
+                        if (expandedSet.size > 0) {
+                            item[attrToExpand + '_options'] = Array.from(expandedSet);
+                        }
+                    }
+                }
+
+                const configurableOptions = await this.api.configurableOptions.list(item.sku);
+                let subPromises = [];
+                item.configurable_options = configurableOptions;
+
+                for (let option of item.configurable_options) {
+                    let atrKey = util.format(CacheKeys.CACHE_KEY_ATTRIBUTE, option.attribute_id);
+                    subPromises.push(new Promise ((resolve, reject) => {
+                        logger.info(`Configurable options for ${atrKey}`);
+                        this.cache.get(atrKey, (err, serializedAtr) => {
+                            let atr = JSON.parse(serializedAtr); // category object
+                            if (atr != null) {
+                                option.attribute_code = atr.attribute_code;
+                                option.values.map((el) => {
+                                    el.label = optionLabel(atr, el.value_index);
+                                });
+
+                                logger.info(`Product options for ${atr.attribute_code} for ${item.sku} set`);
+                                item[atr.attribute_code + '_options'] = option.values.map((el) => { return el.value_index } )
+                            }
+
+                            resolve(item);
+                        });
+                    }));
+                }
+
+                await Promise.all(subPromises);
+                logger.info('Configurable children expanded on product: ', item.sku);
+                return item;
+            }
+        } catch (e) {
+            logger.warn(`Unable to retrieve configurable options info`);
+            return item;
+        }
+    }
+
+    /**
+     * Expands info about categories in product -
+     * In native Magento product have only info about the category ids.
+     * Reindexer appends extra info about the categories product belongs to
+     * @param item Current product
+     * @returns {Promise<Product>}
+     */
+    async processCategories (item) {
+        try {
+            const key = util.format(CacheKeys.CACHE_KEY_PRODUCT_CATEGORIES, item.sku);
+            const getCatsFromCacheToPromise = () => new Promise((resolve) => {
+                this.cache.smembers(key, async (err, categories) => {
+                    if (categories == null) { resolve(item); }
+                    else {
+                        const category = await this._expandCategories(categories);
+                        Object.assign(item, { category });
+                        resolve(item);
+                    }
+                });
+            });
+
+            if (item.category_ids && Array.isArray(item.category_ids) && item.category_ids.length > 0) {
+                const catIdsArray = item.category_ids.map(item => parseInt(item));
+                const category = await this._expandCategories(catIdsArray);
+                Object.assign(item, { category });
+                delete item['category_ids'];
+                return item;
+            } else {
+                delete item['category_ids'];
+                return await getCatsFromCacheToPromise();
+            }
+        } catch (e) {
+            logger.warn('Cannot expand category info: ', e);
+            return item;
+        }
+    }
+
+    async _expandCategories (categoriesIds) {
+        let catPromises = new Array();
+        for (let catId of categoriesIds) {
+            catPromises.push(
+                new Promise((resolve) => {
+                    this.cache.get(util.format(CacheKeys.CACHE_KEY_CATEGORY, catId), (err, serializedCat) => {
+                        let cat = JSON.parse(serializedCat); // category object
+                        if (cat != null) {
+                            resolve({
+                                category_id: cat.id,
+                                name: cat.name,
+                                slug: cat.slug,
+                                path: cat.url_path
+                            })
+                        } else {
+                            resolve({
+                                category_id: catId
+                            });
+                        }
+                    });
+                })
+            );
+        }
+
+        return await Promise.all(catPromises);
     }
 
     /**
