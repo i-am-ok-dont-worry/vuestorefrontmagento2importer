6959eb42781a95a42e6ccd7c3c4b3cfdc2ed171d
Mateusz <mateusz.pytel@grupakmk.pl>
2021-03-21 22:33:28 +0100

    Check for hits in getProductSkus

--- src/adapters/nosql/elasticsearch.js
+++ src/adapters/nosql/elasticsearch.js
@@ -202,7 +202,9 @@ class ElasticsearchAdapter extends AbstractNosqlAdapter {
     const searchToPromise = (query) => new Promise((resolve, reject) => {
       this.db.search(query, (err, res) => {
         scrollId = res.body['_scroll_id'];
-        scrollSize = res.body['hits']['total']['value'];
+        if (res && res.body && res.body.hits && res.body.hits.total) {
+          scrollSize = res.body['hits']['total']['value'];
+        }
 
         if (err) { reject(err); }
         if (res.body.hits) {
