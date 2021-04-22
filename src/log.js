54a1e716775f3ef7b79683af85812e1d572a28a6
Mateusz <mateusz.pytel@grupakmk.pl>
2021-04-22 10:52:07 +0200

    Logs fixed

--- src/log.js
+++ src/log.js
@@ -1,12 +1,25 @@
 var winston = require('winston');
+const { format } = require('winston');
 
 winston.emitErrs = true;
+
+function filterMessagesFormat(filterFunc) {
+  const formatFunc = (info) => {
+    if (filterFunc(info.message)) return info;
+    return null;
+  };
+
+  const format = logform.format(formatFunc);
+  format.transform = formatFunc;
+
+  return format;
+}
 
 if(!global.logger) {
   global.logger = new winston.Logger({
     transports: [
       new winston.transports.Console({
-        level: 'debug',
+        level: 'info',
         handleExceptions: false,
         json: false,
         prettyPrint: true,
