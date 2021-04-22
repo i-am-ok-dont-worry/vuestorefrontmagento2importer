89e4a6d1de3b43a4252bd355ce9c60a6bc949fb9
Mateusz <mateusz.pytel@grupakmk.pl>
2021-04-22 12:42:00 +0200

    special price 0

--- src/adapters/magento/product_new.js
+++ src/adapters/magento/product_new.js
@@ -133,7 +133,7 @@ class ProductNewAdapter extends AbstractMagentoAdapter {
     processPrice (item) {
         try {
             if (!item.hasOwnProperty('special_price')) {
-                item.special_price = 0;
+                item.special_price = '0.0';
             }
         } catch (e) {}
 
